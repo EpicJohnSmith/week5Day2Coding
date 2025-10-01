943d8636210ffcffc15369ee87bf31c230e4e7bb
Julian Young <julian.young@cuw.edu>
2025-10-01 17:13:29 -0500

    Update 1.0

--- src/benchmark/ComparisonSort.java
+++ src/benchmark/ComparisonSort.java
@@ -3,6 +3,37 @@ package benchmark;
 import java.util.Random;
 import java.util.Arrays;
 
-public class ComparisonSort {
-
+public class ComparisonSort
+{
+	public static int[] RandomizedArray(int size, int start, int end)
+	{
+        Random rand = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = rand.nextInt(end - start + 1) + start;
+        }
+        return arr;
+    }
+	
+	// Need bubble sort now
+	class BubbleSort // Used GeeksforGeeks Documentation
+	{  
+	    void bubbleSort(int arr[])
+	    {
+	        int n = arr.length;
+	      
+	        for (int i = 0; i < n - 1; i++)
+	            for (int j = 0; j < n - i - 1; j++)
+	                if (arr[j] > arr[j + 1]) {
+	                  
+	                    // swap temp and arr[i]
+	                    int temp = arr[j];
+	                    arr[j] = arr[j + 1];
+	                    arr[j + 1] = temp;
+	                }
+	    }
+	 
+	    
+	  
 }
